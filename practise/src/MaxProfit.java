147d718f510a1217927e21cd86b30d82ea0db0aa
chenxiao <1270361521@qq.com>
2023-04-29 20:12:14 +0800

    2023/4/29 给定一个数组 prices ，它的第i个元素prices[i] 表示一支给定股票第 i 天的价格。 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。

---
+++ practise/src/MaxProfit.java
@@ -0,0 +1,45 @@
+public class MaxProfit {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int[] prices1 = {7,6,4,3,1};
+        System.out.println(maxProfit1(prices1));
+
+
+    }
+
+    /**
+     * 给定一个数组 prices ，它的第i个元素prices[i] 表示一支给定股票第 i 天的价格。
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     * @param prices
+     * @return
+     */
+    public static int maxProfit(int[] prices) {
+        int len = prices.length;
+        int profit = 0;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (prices[j] - prices[i] > profit) {
+                    profit = prices[j] - prices[i];
+                }
+            }
+        }
+        return profit;
+    }
+
+    public static int maxProfit1(int[] prices) {
+        int len = prices.length;
+        if (len <= 1) return 0;
+        int profit = 0;
+        int min = prices[0];
+        for (int i = 0; i < len; i++) {
+            if (prices[i] < min){
+                min = prices[i];
+            }
+            if (prices[i]-min>profit){
+                profit = prices[i]-min;
+            }
+        }
+        return profit;
+    }
+}
