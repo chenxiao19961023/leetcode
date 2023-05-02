fa782a64236be3ac41b2bddc92117f98a09e98a6
chenxiao <1270361521@qq.com>
2023-05-02 18:23:49 +0800

    2023/5/02 存在重复元素 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。

---
+++ practise/src/ContainsDuplicate.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+//        System.out.println(containsDuplicate(nums));
+    }
+
+    /**
+     * 存在重复元素
+     * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+     * @param nums
+     * @return
+     */
+    public static boolean containsDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean containsDuplicate1(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsDuplicate2(int[] nums) {
+        return Arrays.stream(nums).distinct().count() == nums.length ? false : true;
+    }
+
+    public static boolean containsDuplicate3(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1])
+                return true;
+        }
+        return false;
+    }
+
+}
