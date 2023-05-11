dfddcf5002b84d8fbe0e4f95c638c1ee53ad93fe
chenxiao <1270361521@qq.com>
2023-05-11 21:04:41 +0800

    2023/5/11 数组拆分

--- practise/src/MatrixReshape.java
+++ practise/src/MatrixReshape.java
@@ -2,13 +2,13 @@ import java.util.Arrays;
 
 public class MatrixReshape {
     public static void main(String[] args) {
-        int[][] nums = {{1, 2}, {3,4}};
+        int[][] nums = {{1, 2}, {3, 4}};
         int r = 1;
         int c = 4;
         int[][] ints = matrixReshape(nums, r, c);
         for (int i = 0; i < ints.length; i++) {
             for (int j = 0; j < ints[0].length; j++) {
-                System.out.print(ints[i][j]+" ");
+                System.out.print(ints[i][j] + " ");
             }
             System.out.println();
         }
@@ -19,6 +19,7 @@ public class MatrixReshape {
      * 你一个由二维数组 mat 表示的m x n 矩阵，以及两个正整数 r 和 c ，分别表示想要的重构的矩阵的行数和列数。
      * 重构后的矩阵需要将原始矩阵的所有元素以相同的 行遍历顺序 填充。
      * 如果具有给定参数的 reshape 操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+     *
      * @param mat
      * @param r
      * @param c
@@ -42,5 +43,18 @@ public class MatrixReshape {
         }
         return num;
     }
+
+    public static int[][] matrixReshape1(int[][] mat, int r, int c) {
+        if (mat.length * mat[0].length != r * c) {
+            return mat;
+        }
+        int[][] num = new int[r][c];
+        int m = mat[0].length;
+        for (int i = 0; i < r * c; i++) {
+            num[i / c][i % c] = mat[i / m][i % m];
+        }
+        return num;
+    }
+
 
 }
