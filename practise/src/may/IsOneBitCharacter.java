2d21939373e2e4139eb9089a682bace148b1f167
chenxiao <1270361521@qq.com>
2023-05-19 23:53:24 +0800

    2023/5/19 寻找数组的中心下标

---
+++ practise/src/may/IsOneBitCharacter.java
@@ -0,0 +1,28 @@
+package may;
+
+public class IsOneBitCharacter {
+    public static void main(String[] args) {
+        int[] bits = {1, 0, 0, 0, 0, 0};
+        int[] bits1 = {1, 1, 1, 0};
+        System.out.println(isOneBitCharacter(bits));
+    }
+
+    /**
+     * 1 比特与 2 比特字符
+     * 有两种特殊字符：
+     * 第一种字符可以用一比特0 表示
+     * 第二种字符可以用两比特（10或11）表示
+     * 给你一个以 0 结尾的二进制数组bits，如果最后一个字符必须是一个一比特字符，则返回 true 。
+     * @param bits
+     * @return
+     */
+    public static boolean isOneBitCharacter(int[] bits) {
+        int i;
+        for (i = 0; i < bits.length - 1; i++) {
+            if (bits[i] == 1) {
+                i++;
+            }
+        }
+        return i == bits.length - 1;
+    }
+}
